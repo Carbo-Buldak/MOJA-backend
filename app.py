c2dddca4fde63d6371f058d887c8de3e6bf8588d
sleepyeepanda <sleepyeepanda@gmail.com>
2020-06-15 14:05:47 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restplus import Api
+
+app = Flask(__name__)
+api = Api(app, version='1.0', title='API title', description='A simple API')
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
