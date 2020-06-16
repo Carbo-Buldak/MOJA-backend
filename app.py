eb74ef5c2054ea913e814d2345e7f8dc728c217f
SoHyeonAn <46175598+sleepyeePanda@users.noreply.github.com>
2020-06-16 15:38:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 #from flask_restplus import Api
 
-# app = Flask(__name__)
+app = Flask(__name__)
 # api = Api(app, version='1.0', title='API title', description='A simple API')
 
 @app.route('/')
