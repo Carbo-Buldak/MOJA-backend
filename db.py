d60260ed831e51cdf4f0097846ca82f91cf89ae8
sleepyeepanda <sleepyeepanda@gmail.com>
2020-07-09 09:27:56 +0900

    [ADD] mongoDB 연결 기능 추가

---
+++ db.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import config
+
+client = MongoClient(config.config['mongodb'][0]['url'])
+mongodb = client.MOJA
+
+#mongodb.videos.create_index([('title', 'text')], name='search_index')
+
+def get_all_document(cursor, apply=None):
+    output = []
+    for document in cursor:
+        output.append(document)
+    if apply:
+        output = list(map(apply, output))
+    return output
